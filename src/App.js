0e89868499314a590969e09fc662ecedfc711f57
Test <oleksandr.kolodiazhnyi@takeoff.com>
2021-11-09 11:54:30 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom"
+import Home from "./routes/Home"
+import Registry from "./routes/Registry"
+
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact>
+              <Home/>
+            </Route>
+            <Route path="/registry">
+              <Registry/>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
